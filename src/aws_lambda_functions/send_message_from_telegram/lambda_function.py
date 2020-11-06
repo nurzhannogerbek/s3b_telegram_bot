f7e9f57e6db261f81f292b8dff905a7366b9c979
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-11-06 18:05:20 +0600

    BT-92

--- src/aws_lambda_functions/send_message_from_telegram/lambda_function.py
+++ src/aws_lambda_functions/send_message_from_telegram/lambda_function.py
@@ -18,9 +18,8 @@ TELEGRAM_API_URL = "https://api.telegram.org/bot{0}/".format(TELEGRAM_BOT_TOKEN)
 
 
 def lambda_handler(event, context):
-    print(event)
     response = {
         'statusCode': 200,
-        'body': 'Hello World!'
+        'body': event
     }
     return response
