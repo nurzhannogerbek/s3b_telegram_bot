b8757595b77c953824adcb900c46cf75d17fde20
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-11-06 12:57:37 +0600

    BT-92

---
+++ src/aws_lambda_functions/send_message_from_telegram/lambda_function.py
@@ -0,0 +1,25 @@
+import os
+
+
+# Define global variables.
+CASSANDRA_USERNAME = os.environ["CASSANDRA_USERNAME"]
+CASSANDRA_PASSWORD = os.environ["CASSANDRA_PASSWORD"]
+CASSANDRA_HOST = os.environ["CASSANDRA_HOST"].split(',')
+CASSANDRA_PORT = int(os.environ["CASSANDRA_PORT"])
+CASSANDRA_LOCAL_DC = os.environ["CASSANDRA_LOCAL_DC"]
+CASSANDRA_KEYSPACE_NAME = os.environ["CASSANDRA_KEYSPACE_NAME"]
+POSTGRESQL_USERNAME = os.environ["POSTGRESQL_USERNAME"]
+POSTGRESQL_PASSWORD = os.environ["POSTGRESQL_PASSWORD"]
+POSTGRESQL_HOST = os.environ["POSTGRESQL_HOST"]
+POSTGRESQL_PORT = int(os.environ["POSTGRESQL_PORT"])
+POSTGRESQL_DB_NAME = os.environ["POSTGRESQL_DB_NAME"]
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_API_URL = "https://api.telegram.org/bot{0}/".format(TELEGRAM_BOT_TOKEN)
+
+
+def lambda_handler(event, context):
+    print(event)
+    response = {
+        'statusCode': 200
+    }
+    return response
