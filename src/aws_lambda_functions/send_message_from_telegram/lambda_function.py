76178be2e50ae6f66bd80d89ce6e0f5fc1741083
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-12-25 17:37:51 +0600

    BT-327

--- src/aws_lambda_functions/send_message_from_telegram/lambda_function.py
+++ src/aws_lambda_functions/send_message_from_telegram/lambda_function.py
@@ -606,6 +606,7 @@ def create_chat_room_message(**kwargs):
             }
         ) {
             channelId
+            channelTypeName
             chatRoomId
             chatRoomStatus
             localMessageId
